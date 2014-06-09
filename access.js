c35f70824169d35c555df46d8fb570cc3882bba2
Hobson Lane <hobsonlane@gmail.com>
2014-06-08 22:06:13 -0700

    new access.js for cookie-based login

---
+++ access.js
@@ -0,0 +1,29 @@
+var strCookie = document.cookie;
+var found = false;
+var loc1, loc2;
+var i = 0;
+
+while(i <= strCookie.length)
+{
+  loc1 = i;
+  loc2 = loc1 + 11;
+
+  if(strCookie.substring(loc1,loc2) == "access=true")
+  {
+      found = true;
+      break;
+  }
+  i++;
+}
+
+if(!found)
+{
+   document.location = "login.html";
+}
+
+function deleteCookie()
+{
+   var d = new Date(2014, 06, 22);
+   document.cookie = "access=false;expires="  + d.toGMTString() + "; path=/";
+   document.location = "login.htm";
+}
