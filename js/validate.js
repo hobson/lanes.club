5f83d5cae6df8574c2b3f57281f4854b823928f8
Hobson Lane <hobsonlane@gmail.com>
2014-06-09 20:28:13 -0700

    lots of destructive edits

--- js/validate.js
+++ js/validate.js
@@ -3,11 +3,11 @@
 function validate(form)
 {
   console.log(Sha256.hash(form.password.value))
-  if(Sha256.hash(form.password.value) == 'password')
+  if(Sha256.hash(form.password.value) == '9c9e85162ea714280c952ab799ccf6a15921309d8c6520293de24d7703d83668')
   {
     console.log('validated!!!')
      var date = new Date();
-     date.setTime(date.getTime() + (2 * 365 * 24 * 3600 * 1000));
+     date.setTime(date.getTime() + (14 * 24 * 3600 * 1000));
      var expires = "; expires=" + date.toGMTString();
      document.cookie = "access=true" + expires + "; path=/";
      document.location =  "index.html";
