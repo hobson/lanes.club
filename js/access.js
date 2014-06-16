62944a555f7c262f9555217746b715cd59e64007
Hobson Lane <hobsonlane@gmail.com>
2014-06-15 18:49:45 -0700

    modularized access.js a little

--- js/access.js
+++ js/access.js
@@ -3,42 +3,41 @@ var found = false;
 var loc1, loc2;
 var i = 0;
 
-console.log(strCookie);
+function get_relative_url(levels_up) {
+    if levels_up == null) { levels_up = 2; }
 
-while(i <= strCookie.length)
-{
-  loc1 = i;
-  loc2 = loc1 + 11;
-  console.log(strCookie.substring(loc1,loc2))
-  if(strCookie.substring(loc1,loc2) == "access=true")
-  {
-      console.log("found at " + loc1 + ", " + loc2)
-      found = true;
-      break;
-  }
-  i++;
-}
+    var pathElements = window.location.pathname.split('/');
+    var newPathname = "";
+    for (i = 0; i < pathArray.length-levels_up; i++) {
+        newPathname += "/";
+        newPathname += pathArray[i];
+    }
+    if newPathname.length > 0) { newPathname += "/"; }
+    var newURL = window.location.protocol + "//" + window.location.host + "/" + newPathname;
+    return newURL;
+    }
 
-var pathElements = window.location.pathname.split('/');
-var newPathname = "";
-for (i = 0; i < pathArray.length-2; i++) {
-    newPathname += "/";
-    newPathname += pathArray[i];
-}
-if newPathname.length > 0) {
-    newPathname = newPathname + '/';
-}
-var newURL = window.location.protocol + "//" + window.location.host + "/" + newPathname + 'login.html';
+newURL = get_relative_url(2)
 
-console.log('found = ' + found)
-if(!found)
-{
-    document.location = newURL;
-}
+function deleteCookie() {
+    var d = new Date(2014, 06, 09);
+    document.cookie = "access=false;expires="  + d.toGMTString() + "; path=/";
+    document.location = newURL + "login.html";
+    }
 
-function deleteCookie()
-{
-   var d = new Date(2014, 06, 09);
-   document.cookie = "access=false;expires="  + d.toGMTString() + "; path=/";
-   document.location = "/login.html";
-}
+while(i <= strCookie.length) {
+    loc1 = i;
+    loc2 = loc1 + 11;
+    // console.log(strCookie.substring(loc1,loc2))
+    if(strCookie.substring(loc1,loc2) == "access=true") {
+        // console.log("found at " + loc1 + ", " + loc2)
+        found = true;
+        break;
+        }
+    i++;
+    }
+
+// console.log('found = ' + found)
+if(!found) {
+    document.location = newURL + "login.html";
+    }
